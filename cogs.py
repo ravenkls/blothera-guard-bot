84f10f417924eaa494baf422eeda60bd56188776
ravenkls <kristian.smith.2001@hotmail.co.uk>
2019-03-22 20:00:33 +0000

    atlas integration

--- cogs.py
+++ cogs.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from pathlib import Path
+import aiohttp
 
 RULES_CHANNEL_ID = 554695025485807647
 
@@ -14,8 +15,27 @@ class WelcomeCog(commands.Cog):
         rules = member.guild.get_channel(RULES_CHANNEL_ID)
         await member.send(self.welcome_message.format(member=member, guild=member.guild, rules=rules))
 
+
+class AtlasCog(commands.Cog):
     
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def nations(self, ctx):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://mc-atlas.com/nation/api/nation/list') as resp:
+                response = resp.json()
+        if response['Status'] == 'OK':
+            nations_names = [nation['nationName'] for nation in response['Data']['nationList']]
+            await ctx.send('*These are all the nations on Atlas according to our records:' + '\n'.join([
+                ' **-** ' + n for n in nations_names]))
+        else:
+            await ctx.send('*Hmmm, I can\'t seem to find our records at the moment, check back later!')
+
+
 def setup(bot):
     with open(Path('messages/welcome/welcome_message.txt')) as msg_file:
         msg = msg_file.read()
     bot.add_cog(WelcomeCog(bot, welcome_message=msg))
+    bot.add_cog(AtlasCog(bot))
