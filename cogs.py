9087c3557a5c95544a39a83ba825bb14592bdc46
ravenkls <kristian.smith.2001@hotmail.co.uk>
2019-03-22 20:21:59 +0000

    nation info command

--- cogs.py
+++ cogs.py
@@ -26,11 +26,28 @@ class AtlasCog(commands.Cog):
         response = requests.get('https://mc-atlas.com/nation/api/nation/list').json()
         if response['Status'] == 'OK':
             nations_names = [nation['nationName'] for nation in response['Data']['nationList']]
-            await ctx.send('*These are all the nations on Atlas according to our records:*\n' + '\n'.join([
-                ' **-** ' + n for n in nations_names]))
+            await ctx.send(('*These are all the nations on Atlas according to our records:*\n' + '\n'.join([
+                ' **-** ' + n for n in nations_names])[:2000]))
         else:
             await ctx.send('*Hmmm, I can\'t seem to find our records at the moment, check back later!')
 
+    @commands.command()
+    async def nation(self, ctx, name):
+        response = requests.get('https://mc-atlas.com/nation/api/nation/list').json()
+        if response['Status'] == 'OK':
+            for nation in response['Data']['nationList']:
+                if nation['nationName'].lower().strip() == name.lower().strip():
+                    owner = ', '.join([p for p in nation['citizens'] if p['isLeader']])
+                    if not owner:
+                        owner = 'N/A'
+                    break
+            else:
+                return await ctx.send(f'I couldn\'t find any nation by the name of {name}')
+            string = (f'**{nation["nationName"]}**\n'
+                      f'Owner: {owner}'
+                      f'Number of Towns: {len(nation["towns"])}\n'
+                      f'Number of Citizens: {len(nation["citizens"])}')
+        
 
 def setup(bot):
     with open(Path('messages/welcome/welcome_message.txt')) as msg_file:
