c065866e6c02f2710444720cc215fdf8c660efd6
ravenkls <kristian.smith.2001@hotmail.co.uk>
2019-03-23 12:58:51 +0000

    nation embeds lok nicer

--- cogs.py
+++ cogs.py
@@ -1,9 +1,14 @@
 from discord.ext import commands
 from pathlib import Path
+import datetime
 import requests
+import humanize
+import discord
+
 
 RULES_CHANNEL_ID = 554695025485807647
 GOV_DOCS_CHANNEL_ID = 553795615042306079
+
 
 class WelcomeCog(commands.Cog):
     
@@ -24,32 +29,46 @@ class AtlasCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def nations(self, ctx):
-        response = requests.get('https://mc-atlas.com/nation/api/nation/list').json()
-        if response['Status'] == 'OK':
-            nations_names = [nation['nationName'] for nation in response['Data']['nationList']]
-            await ctx.send(('*These are all the nations on Atlas according to our records:*\n' + '\n'.join([
-                ' **-** ' + n for n in nations_names])[:2000]))
-        else:
-            await ctx.send('*Hmmm, I can\'t seem to find our records at the moment, check back later!')
+    async def nations(self, ctx, *, name: str=''):
+        if not name:
+            response = requests.get('https://mc-atlas.com/nation/api/nation/list').json()
+            if response['Status'] == 'OK':
+                nations_names = [nation['nationName'] for nation in response['Data']['nationList']]
+                
+                embed = discord.Embed(title='Atlas Nation List', colour=0xc62323,
+                                    description='\n'.join(['- ' + n for n in nations_names]))
 
-    @commands.command()
-    async def nation(self, ctx, *, name: str):
-        response = requests.get('https://mc-atlas.com/nation/api/nation/list', params={'ShowCitizens': True, 'ShowTowns': True}).json()
-        if response['Status'] == 'OK':
-            for nation in response['Data']['nationList']:
-                if nation['nationName'].lower().strip() == name.lower().strip():
-                    owner = ', '.join([p['userName'] for p in nation['citizens'] if p['isLeader']])
-                    if not owner:
-                        owner = 'N/A'
-                    break
+                await ctx.send('*These are all the nations on Atlas according to our records*', embed=embed)
             else:
-                return await ctx.send(f'I couldn\'t find any nation by the name of {name}')
-            string = (f'*Heres what I found on* ***{nation["nationName"]}***\n\n'
-                      f'Owner: {owner}\n'
-                      f'Number of Towns: {len(nation["towns"])}\n'
-                      f'Number of Citizens: {len(nation["citizens"])}')
-            await ctx.send(string)
+                await ctx.send('*Hmmm, I can\'t seem to find our records at the moment, check back later!')
+        else:
+            response = requests.get('https://mc-atlas.com/nation/api/nation/list', params={'ShowCitizens': True, 'ShowTowns': True}).json()
+            if response['Status'] == 'OK':
+                for nation in response['Data']['nationList']:
+                    if nation['nationName'].lower().strip() == name.lower().strip():
+                        owners = [p for p in nation['citizens'] if p['isLeader']]
+                        owner = {'userName': 'no one', 'userUuid': 'steve'} if not owners else owners[0]
+                        break
+                else:
+                    return await ctx.send(f'I couldn\'t find any nation by the name of {name}')
+                
+
+                embed = discord.Embed(title=nation['nationName'], colour=0xc62323,
+                                    description=f"The nation {nation['nationName']} is owned by {owner['userName']}")
+                embed.set_thumbnail(url="https://minotar.net/avatar/" + owner['userUuid'])
+                
+                last_seen = sorted(nation['citizens'], key=lambda x: x['userLastSeenTime'], reverse=True)
+                last_seen_string = ''
+                for player in last_seen[:10]:
+                    natural = humanize.naturaltime(datetime.datetime.fromtimestamp(player['userLastSeenTime']/1000))
+                    last_seen_string += f'- {player["userName"]} (last seen {natural})\n'
+                towns_string = '\n'.join(['- ' + t['townName'] for t in nation['towns']]) or 'N/A'
+                last_seen_string = last_seen_string or 'N/A'
+
+                embed.add_field(name="Towns", value=towns_string)
+                embed.add_field(name="Citizens", value=last_seen_string, inline=False)
+
+                await ctx.send(content='*Let me just find my notes...*', embed=embed)
 
 def setup(bot):
     with open(Path('messages/welcome/welcome_message.txt')) as msg_file:
